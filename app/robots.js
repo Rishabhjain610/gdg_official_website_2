bd8d9d765150f6265bac3e77b70f4328d052e442
Rishabh Jain <jainrishabh2610@gmail.com>
2025-11-05 15:42:09 +0530

    allow_All_bots

--- app/robots.js
+++ app/robots.js
@@ -1,7 +1,18 @@
 export default function robots() {
   const base = "https://gdgtsecoffical.dpdns.org";
   return {
-    rules: [{ userAgent: "*", allow: "/" }],
+    rules: [
+      // Allow all bots by default
+      { userAgent: "*", allow: "/" },
+      // Explicitly allow major AI bots
+      { userAgent: "GPTBot", allow: "/" },
+      { userAgent: "ClaudeBot", allow: "/" },
+      { userAgent: "Google-Extended", allow: "/" },
+      { userAgent: "Amazonbot", allow: "/" },
+      { userAgent: "Applebot-Extended", allow: "/" },
+      { userAgent: "Bytespider", allow: "/" },
+      { userAgent: "meta-externalagent", allow: "/" },
+    ],
     sitemap: `${base}/sitemap.xml`,
     host: base,
   };
